7fe89c7edb255ddf8fe5f7af1cf8a81d34e2f055
zhangxing <zhangxing_956@163.com>
2018-03-02 17:37:47 +0800

    up

--- pages/word/word.js
+++ pages/word/word.js
@@ -1,6 +1,7 @@
 // pages/word/word.js
 const app = getApp()
 const sheet = require('../../sheets.js')
+import { doFetch } from '../../utils/rest.js';
 
 Page({
 
@@ -8,17 +9,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    knowledgePoint: [{ img: 'https://gengxin.odao.com/update/h5/yingyu/word/noun.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/noun-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/noun-small.png', name:'名词', up:true },
-      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/adj.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/adj-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/adj-small.png', name: '形容词', up: true },
-      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/adv.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/adv-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/adv-small.png', name: '副词', up: false },
-      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/pron.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/pron-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/pron-small.png', name: '代名词', up: true },
-      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/num.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/num-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/num-small.png', name: '数词', up: false },
-      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/verb.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/verb-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/verb-small.png', name: '动词', up: false },
-      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/art.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/art-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/art-small.png', name: '冠词', up: false },
-      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/prep.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/prep-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/prep-small.png', name: '介系词', up: false },
-      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/conj.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/conj-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/conj-small.png', name: '连接词', up: true },
-      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/int.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/int-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/int-small.png', name: '感叹词', up: true }
+    knowledgePoint: [{ img: 'https://gengxin.odao.com/update/h5/yingyu/word/noun.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/noun-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/noun-small.png', name:'名词',},
+      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/adj.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/adj-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/adj-small.png', name: '形容词',},
+      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/adv.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/adv-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/adv-small.png', name: '副词',},
+      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/pron.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/pron-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/pron-small.png', name: '代名词',},
+      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/num.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/num-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/num-small.png', name: '数词',},
+      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/verb.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/verb-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/verb-small.png', name: '动词',},
+      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/art.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/art-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/art-small.png', name: '冠词',},
+      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/prep.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/prep-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/prep-small.png', name: '介系词',},
+      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/conj.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/conj-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/conj-small.png', name: '连接词',},
+      { img: 'https://gengxin.odao.com/update/h5/yingyu/word/int.png', big: 'https://gengxin.odao.com/update/h5/yingyu/word/int-big.png', small: 'https://gengxin.odao.com/update/h5/yingyu/word/int-small.png', name: '感叹词',}
     ],
+    knowInfo: {},
     point: 0,  //选择的知识点
     show:false,  //显示知识点信息
     canUpdate: false,  //知识点是否可以升级
@@ -38,6 +40,12 @@ Page({
     })
     this.setData({
       descript: dsc
+    })
+    doFetch('english.develop',{},(res)=>{
+      console.log(res.data);
+      this.setData({
+        knowInfo: res.data
+      })
     })
   },
 
@@ -85,15 +93,30 @@ Page({
 
   //查看知识点具体信息
   check: function (res) {
+    let ind = res.currentTarget.dataset.ind;
+    if (this.data.knowInfo[ind+1].level == this.data.maxLevel){
+      this.setData({
+        isMax:true
+      })
+    }
     this.setData({
-      point: res.currentTarget.dataset.ind,
-      show:true
+      point: ind,
+      show:true,
+      canUpdate: this.data.knowInfo[ind + 1].canUp
     })
   },
 
   improve: function() {
     if(this.data.canUpdate){
-
+      doFetch('english.speechlevelup',{
+        spid:this.data.point+1
+      },(res)=>{
+        let upKnowInfo = this.data.knowInfo;
+        upKnowInfo[this.data.point+1] = res.data;
+        this.setData({
+          knowInfo:upKnowInfo
+        })
+      })
     }
     else{
       this.setData({
