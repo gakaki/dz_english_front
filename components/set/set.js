796e44db9f69a94696924a7df4e8b4f1893bc689
zhangxing <zhangxing_956@163.com>
2018-03-07 15:32:36 +0800

    up

--- components/set/set.js
+++ components/set/set.js
@@ -11,13 +11,42 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    send:'',
+    music:'',
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    sendOpen() {
+      if (wx.getStorageSync('send')){
+        this.setData({
+          send: 'send-open'
+        })
+        wx.setStorageSync('send', false);
+      }
+      else{
+        this.setData({
+          send: 'send-close'
+        })
+        wx.setStorageSync('send', true);
+      }
+      
+    },
+    musicOpen() {
+      if (wx.getStorageSync('music')) {
+        this.setData({
+          music: 'send-open'
+        })
+        wx.setStorageSync('music', false);
+      }
+      else {
+        this.setData({
+          music: 'send-close'
+        })
+        wx.setStorageSync('music', true);
+      }
+    }
   }
 })
