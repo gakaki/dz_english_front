9a1d88aafa83139fa4809c6bb857c40c2ef6a5dd
swellee <shiwei_li@odao.com>
2018-03-09 14:12:15 +0800

    up

--- utils/rest.js
+++ utils/rest.js
@@ -1,24 +1,11 @@
 const io = require('./index.js');
-<<<<<<< HEAD
-const srv = "https://h5t.ddz2018.com/";
-const wss = "wss://h5t.ddz2018.com/english";
-// const srv = "https://local.ddz2018.com/";
-// const wss = "wss://local.ddz2018.com/english";
-=======
-<<<<<<< HEAD
-const srv = "https://h5t.ddz2018.com/";
-const wss = "wss://h5t.ddz2018.com/english";
-//const srv = "https://local.ddz2018.com/";
-//const wss = "wss://local.ddz2018.com/english";
-=======
 
 //const srv = "https://h5t.ddz2018.com/";
 //const wss = "wss://h5t.ddz2018.com/english";
 const srv = "https://local.ddz2018.com/";
 const wss = "wss://local.ddz2018.com/english";
 
->>>>>>> b2255810d4d474a6b5cef3bcf78ab856c1afe180
->>>>>>> f95ec3fb8f3bcea79b90c764d585bfe74ffaa3f0
+
 const care = require('./util.js');
 const CODE_SUC = 0;
 const APPNAME = 'english';
