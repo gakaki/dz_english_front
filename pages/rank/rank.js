a527c015c92d57fbba5fa337792a361f7bc6d469
zhangqi <qii_zhang@odao.com>
2018-03-12 22:02:32 +0800

    up

--- pages/rank/rank.js
+++ pages/rank/rank.js
@@ -24,11 +24,12 @@ Page({
           rankData: res.data.map(this.getSegment)
         })
 
+       
       } else {
         wx.showToast({
           title: '暂无数据',
           icon: 'none',
-          duration: 2000,
+          duration: 500,
           mask: true
         })
       }
@@ -36,6 +37,9 @@ Page({
   },
   getSegment: function (item) {
     item.rank = sheet.Stage.Get(item.rank).stage
+    if (item.location == ' ' || typeof (item.location) == 'undefined') {
+      item.location = sheet.Constant.Get(3).value.split(",")[Math.floor(Math.random() * 5)] 
+    }
     return item
   },
   toDes() {
@@ -81,7 +85,7 @@ Page({
           wx.showToast({
             title: '暂无数据',
             icon: 'none',
-            duration: 2000,
+            duration: 500,
             mask: true
           })
           this.setData({
@@ -109,7 +113,7 @@ Page({
           wx.showToast({
             title: '暂无数据',
             icon: 'none',
-            duration: 2000,
+            duration: 500,
             mask: true
           })
         }
