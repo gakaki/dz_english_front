11e34ddd33a8c3c9453364474fd1bf0840812908
zhangqi <qii_zhang@odao.com>
2018-03-14 13:33:12 +0800

    up

--- pages/shopping/shopping.js
+++ pages/shopping/shopping.js
@@ -94,8 +94,9 @@ Page({
         package: r.data.payload.package,
         signType: r.data.payload.signType,
         paySign: r.data.payload.paySign,
-        success(r) {
-          app.globalData.personalInfo.userInfo.items = r.data
+        success(res) {
+          console.log(res)
+          app.globalData.personalInfo.userInfo.items = res.data
           
           wx.showToast({
             title: '购买成功',
@@ -103,7 +104,7 @@ Page({
             duration: 2000,
             mask: true
           })
-          wsReceive('getItem', r => {
+          wsReceive('getItem', res => {
            
           })
           
