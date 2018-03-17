fbfab872cbbf449bef03887656b7808017974ef8
yhy <hongyan_yang@odao.com>
2018-03-17 14:02:51 +0800

    删除awaitPK 页面 log

--- pages/awaitPK/awaitPK.js
+++ pages/awaitPK/awaitPK.js
@@ -71,7 +71,6 @@ Page({
       }, 2500)
     })
     wsReceive('matchFailed',res=>{
-      console.log(res,'fail')
       this.setData({
         awaiting:true
       })
@@ -81,14 +80,13 @@ Page({
         duration: 2000
       })
       time = setTimeout(function(){
-        console.log('matchFailed back')
+        
         wx.navigateBack({
           delta: 1
         })
       },2100)
     })
     wsReceive('matchSuccess',res=>{
-      console.log(res,'suc')
       this.setData({
         matchSuc:true
       })
